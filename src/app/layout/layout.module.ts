107b1213e724b1fe73473223f3bf55495f124ece
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-12-29 16:52:49 -0300

    feat: implementando o footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,18 +4,21 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
     LayoutRoutingModule
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class LayoutModule { }
